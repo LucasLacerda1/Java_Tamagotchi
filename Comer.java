6d3c85d0b7ec0ad071764f40fea3020d1065c165
Lucas Lacerda Freire <lacerdafreire86@gmail.com>
2025-09-25 10:55:48 -0300

    Create Comer.java

---
+++ Comer.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Comer {
+    private ArrayList<String> comidas = new ArrayList<String>();
+
+    public Comer() {
+        comidas.add("Tutu de camarão");
+        comidas.add("Bobó de camarão");
+        comidas.add("Risoto de camarão");
+        comidas.add("Camarão na manteiga");
+        comidas.add("Camarão empanado");
+        comidas.add("Strogonofe de camarão");
+        comidas.add("Camarão com molho de queijo");
+        comidas.add("Muqueca com camarão");
+        comidas.add("Lasanha de camarão");
+        comidas.add("Empadão de camarão");
+    }
+    
+    
+}
